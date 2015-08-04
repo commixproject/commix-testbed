aaf7cd787c1e783fcb7ce2738bdca4dca727d486
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2015-08-04 10:36:28 +0300

    New regexp examples.

---
+++ regular/GET/classic_regexp.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+     <title>Command injection test environment</title>
+    </head>
+    <body>
+        <form action="classic_regexp.php" method="GET">
+        Enter your name: <input type="text" name="addr">
+        <input type="submit">
+        </form>
+    </body>
+</html>
+
+<?php
+    $addr = $_GET['addr'];
+    if(isset($addr)){
+        # Inspired from pentesterlab.com - 'Web for Pentester' course.
+        # https://pentesterlab.com/exercises/web_for_pentester
+        if (!(preg_match('/^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/m',$addr))){
+            die("Invalid IP address format.");
+        }else{
+            # Execute command!
+            echo exec("/bin/ping -c 4 ".$addr);
+        }
+    }
+?>
