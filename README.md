bcb41df24a3818353a6a461051a291b94b290009
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2015-08-08 13:08:09 +0300

    New style added :)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Command injection test environment
 A collection of web pages, vulnerable to command injection flaws, used to test [commix](https://github.com/stasinopoulos/commix)'s vulnerability detection and exploitation features.
-* [Regular](https://github.com/stasinopoulos/commix-testbed/tree/master/regular) injection scenarios.
-* [Cookie](https://github.com/stasinopoulos/commix-testbed/tree/master/cookie) injection scenarios.
-* [User-Agent](https://github.com/stasinopoulos/commix-testbed/tree/master/user-agent) injection scenarios.
-* [Referer](https://github.com/stasinopoulos/commix-testbed/tree/master/referer) injection scenarios.
+* [Regular](https://github.com/stasinopoulos/commix-testbed/tree/master/scenarios/regular) injection scenarios.
+* [Cookie](https://github.com/stasinopoulos/commix-testbed/tree/master/scenarios/cookie) injection scenarios.
+* [User-Agent](https://github.com/stasinopoulos/commix-testbed/tree/master/scenarios/user-agent) injection scenarios.
+* [Referer](https://github.com/stasinopoulos/commix-testbed/tree/master/scenarios/referer) injection scenarios.
 
 ####Bugs and Enhancements
 For bug reports or enhancements, please open an issue [here](https://github.com/stasinopoulos/commix-testbed/issues).
---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Commix testbed - Command injection test environment</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- font-awesome Core CSS -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap-submenu.css">
+    <script src="js/bootstrap-submenu.js" defer></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <a class="navbar-brand">
+                    <img style="max-width:43px; margin-top: -11px;"
+                         src="../commix-testbed/img/logo.png">
+                </a>
+                <a class="navbar-brand" href="index.php">commix-testbed (v0.1)</a>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- Page Content -->
+    <div class="container">
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h2><b>Commix testbed - A command injection test environment!</b></h2>
+            <p>A collection of web pages, vulnerable to <b><a href="https://www.owasp.org/index.php/Command_Injection">command injection flaws</a></b>, used to test <b><a href="https://github.com/stasinopoulos/commix">commix</a></b>'s vulnerability <b>detection</b> and <b>exploitation</b> features.</p>
+            <p>
+            <a href="https://twitter.com/commixproject" class="twitter-follow-button" data-show-count="false">Follow @commixproject</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            <iframe src="https://ghbtns.com/github-btn.html?user=stasinopoulos&repo=commix&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
+        <div class="text-center"><!--You can add col-lg-12 if you want -->
+        </div>
+        </header>
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h3>Command injection scenarios categories</h3>
+                <br><br>
+
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Page Features -->
+        <div class="container">
+          <div class="row ">
+              <!-- User-Agent HTTP Header -->
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="panel panel-info">
+                      <div class="panel-heading">
+                        <h4>1. Regular (GET / POST)  </h4>
+                      </div>
+                      <ul class="list-group">
+                          <li class="list-group-item">Classic regular example<br>(<a href="scenarios/regular/GET/classic.php">GET</a> | <a href="scenarios/regular/POST/classic.php"> POST</a>)</li>
+                          <li class="list-group-item">Classic single-quote example<br>(<a href="scenarios/regular/GET/classic_quote.php">GET</a> | <a href="scenarios/regular/POST/classic_quote.php"> POST</a>)</li>                          
+                          <li class="list-group-item">Classic regexp example<br>(<a href="scenarios/regular/GET/classic_regexp.php">GET</a> | <a href="scenarios/regular/POST/classic_regexp.php"> POST</a>)</li>
+                          <li class="list-group-item">Blind regular example<br>(<a href="scenarios/regular/GET/blind.php">GET</a> | <a href="scenarios/regular/POST/blind.php"> POST</a>)</li>
+                          <li class="list-group-item">Blind regexp example<br>(<a href="scenarios/regular/GET/blind_regexp.php">GET</a> | <a href="scenarios/regular/POST/blind_regexp.php"> POST </a>)</li>
+                          <li class="list-group-item">Eval regular example<br>(<a href="scenarios/regular/GET/eval.php">GET</a> | <a href="scenarios/regular/POST/eval.php"> POST </a>)</li>
+                      </ul>
+
+                  </div>
+              </div>
+              <!-- User-Agent HTTP Header -->
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="panel panel-info">
+                      <div class="panel-heading">
+                        <h4>2. User-Agent HTTP Header </h4>
+                      </div>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="scenarios/user-agent/ua(classic).php">Classic user-agent-based example</a></li>
+                            <li class="list-group-item"><a href="scenarios/user-agent/ua(blind).php">Blind user-agent-based example</a></li>        
+                            <li class="list-group-item"><a href="scenarios/user-agent/ua(eval).php">Eval user-agent-based example</a></li>
+                        </ul>
+                  </div>
+              </div>
+              <!-- Cookie Header HTTP Header -->
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="panel panel-info">
+                        <div class="panel-heading">
+                          <h4>3. Cookie HTTP Header</h4>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="scenarios/cookie/cookie(classic).php">Classic cookie-based example</a></li>
+                            <li class="list-group-item"><a href="scenarios/cookie/cookie(blind).php">Blind cookie-based example</a></li>        
+                            <li class="list-group-item"><a href="scenarios/cookie/cookie(eval).php">Eval cookie-based example</a></li>
+                        </ul>
+                  </div>
+              </div>
+              <!-- Referer HTTP Header -->
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="panel panel-info">
+                        <div class="panel-heading">
+                          <h4>4. Referer HTTP Header</h4>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="scenarios/referer/referer(classic).php">Classic referer-based example</a></li>
+                            <li class="list-group-item"><a href="scenarios/referer/referer(blind).php">Blind referer-based example</a></li>       
+                            <li class="list-group-item"><a href="scenarios/referer/referer(eval).php">Eval cookie-based example</a></li>
+                        </ul>
+                   </div>
+              </div>
+          </div>
+        </div>
+        <!-- /.row -->
+        <hr>
+        <!-- Footer -->
+        <footer>
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Anastasios Stasinopoulos</p>
+                    <a href="https://twitter.com/ancst" class="twitter-follow-button" data-show-count="false">Follow @ancst</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
