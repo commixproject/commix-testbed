02198b635e104d6c4458c6050ea9c02613dedd05
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2015-08-12 10:07:03 +0300

    Added blacklisting examples.

--- index.php
+++ index.php
@@ -80,6 +80,7 @@
                           <li class="list-group-item">Classic single-quote example<br>(<a href="scenarios/regular/GET/classic_quote.php">GET</a> | <a href="scenarios/regular/POST/classic_quote.php"> POST</a>)</li>       
                           <li class="list-group-item">Classic double-quote example<br>(<a href="scenarios/regular/GET/classic_double_quote.php">GET</a> | <a href="scenarios/regular/POST/classic_double_quote.php"> POST</a>)</li> 
                           <li class="list-group-item">Classic regexp example<br>(<a href="scenarios/regular/GET/classic_regexp.php">GET</a> | <a href="scenarios/regular/POST/classic_regexp.php"> POST</a>)</li>
+                          <li class="list-group-item">Classic blacklisting example<br>(<a href="scenarios/regular/GET/classic_blacklisting.php">GET</a> | <a href="scenarios/regular/POST/classic_blacklisting.php"> POST</a>)</li>
                           <li class="list-group-item">Classic hashing example<br>(<a href="scenarios/regular/GET/classic_hash.php">GET</a> | <a href="scenarios/regular/POST/classic_hash.php"> POST</a>)</li>
                           <li class="list-group-item">Blind regular example<br>(<a href="scenarios/regular/GET/blind.php">GET</a> | <a href="scenarios/regular/POST/blind.php"> POST</a>)</li>
                           <li class="list-group-item">Blind regexp example<br>(<a href="scenarios/regular/GET/blind_regexp.php">GET</a> | <a href="scenarios/regular/POST/blind_regexp.php"> POST </a>)</li>
