839498a12828af29660875fc70bb22809278e6b3
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2016-01-31 11:56:09 +0200

    Minor fix

--- scenarios/user-agent/ua(blind).php
+++ scenarios/user-agent/ua(blind).php
@@ -69,16 +69,16 @@
                 <br>
                 <b>
                 <?php
-                  if( stristr(php_uname('s'), 'Windows NT')){
+                  if(stristr(php_uname('s'), 'Windows NT')){
                     die("Invalid operating system.");
                   } else {
                     $user_agent = $_SERVER['HTTP_USER_AGENT'];
                     exec("echo '".$user_agent."' | grep Firefox", $output, $return);
-                  }  
-                  if (!$return) {
-                      echo "Viva La Mozilla Firefox!";
-                  } else {
-                      echo "Please, download Mozilla Firefox!";
+                    if (!$return) {
+                        echo "Viva La Mozilla Firefox!";
+                    } else {
+                        echo "Please, download Mozilla Firefox!";
+                    }
                   }
                 ?>
                 </b>
