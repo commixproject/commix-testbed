d121b1d9ec08d18b907f2d480aae3d3a9442cff8
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2016-04-10 18:09:14 +0300

    Minor update

--- scenarios/regular/POST/classic_json.php
+++ scenarios/regular/POST/classic_json.php
@@ -67,22 +67,21 @@
               <!-- User-Agent HTTP Header -->
               <div class="jumbotron hero-spacer">
                 <b>
-		<?php
-		// JSON request format:
-		// {"addr":"127.0.0.1","name":"ancst"}
-		// read JSon input
-		$data_back = json_decode(file_get_contents('php://input'));
-		// set json string to php variables
-		$addr = $data_back->{"addr"};
-		$name = $data_back->{"name"};
-		// create json response
-		$responses = array("Execution Result" => array("Address" => array("IP", $addr),
-						   "Done by" => array("User", $name),
-						   "Result" => array("Output", exec("/bin/ping -c 4 ".$addr))
-						   )
-		);
-		echo json_encode($responses);
-		?>
+                <?php
+                // JSON request format:
+                // {"addr":"127.0.0.1","name":"ancst"}
+                // read JSon input
+                $data_back = json_decode(file_get_contents('php://input'));
+                // set json string to php variables
+                $addr = $data_back->{"addr"};
+                $name = $data_back->{"name"};
+                // create json response
+                $responses = array("Execution Result" => array("Address" => array("IP", $addr),
+                           "Done by" => array("User", $name),
+                           "Result" => array("Output", exec("/bin/ping -c 4 ".$addr))
+                           ));
+                echo json_encode($responses);
+                ?>
                 </b>
               </div> 
           </div>
