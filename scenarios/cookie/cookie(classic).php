796bc289233daacbaddcb334c5856fccc086a29b
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2017-02-22 08:49:12 +0200

    Minor fix

--- scenarios/cookie/cookie(classic).php
+++ scenarios/cookie/cookie(classic).php
@@ -95,7 +95,7 @@
         <footer>
             <div class="row text-center">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Anastasios Stasinopoulos</p>
+                    <p>Made in Greece with <b>&hearts;</b> by <a href="https://github.com/stasinopoulos"> Anastasios Stasinopoulos</a>.</p>
                     <a href="https://twitter.com/ancst" class="twitter-follow-button" data-show-count="false">Follow @ancst</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                 </div>
             </div>
